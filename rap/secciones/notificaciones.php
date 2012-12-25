64984ae2ecb1f3e7c95feb004f72d1d4e8b8cc6e
Neodivert <neodivert@gmail.com>
2012-12-25 20:59:53 +0000

    - En el commit anterior el código se copió en src en vez de moverse. Eliminadas las copias anteriores.

--- rap/secciones/notificaciones.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-	if( !isset( $_GET['pagina'] ) ) $_GET['pagina'] = 0;
-
-	require_once DIR_LIB . 'notificaciones.php';
-
-	GenerarLibro( $_GET['pagina'], array( 'ObtenerUltimasNotificaciones' ), array( 'MostrarNotificacion' ), 25 );
-?>
