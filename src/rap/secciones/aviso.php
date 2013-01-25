f9047cc9d5da1adfa2560c282902dea9f5f40a49
Neodivert <neodivert@gmail.com>
2013-01-25 19:02:03 +0000

    - Corregido error por el que aviso.php siempre mostraba texto y por tanto impedía las redirecciones con header().

--- src/rap/secciones/aviso.php
+++ src/rap/secciones/aviso.php
@@ -4,7 +4,8 @@
 		'AV_PERLA_DENUNCIADA'=> 'Tu voto para eliminar la perla se ha recibido correctamente',
 		'AV_DENUNCIA_ELIMINADA' => 'Tu voto para eliminar la perla se ha eliminado correctamente'
 	);
-
-	echo "{$mensajes[$_GET['mensaje']]}<br/>";
+	if( isset( $_GET['mensaje'] ) ){
+		echo "{$mensajes[$_GET['mensaje']]}<br/>";
+		echo '<a href="general.php?seccion=lista_perlas">Volver a la pagina principal</a>';
+	}
 ?>
-<a href="general.php?seccion=lista_perlas">Volver a la pagina principal</a>
