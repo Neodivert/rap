1b106baa5eb9380ecb7e102658911afdc144142b
neodivert <neodivert@gmail.com>
2013-02-23 17:45:14 +0000

    - Fichero de rutas editado para incluir la ruta a la carpeta de clases.

--- src/rap/php/config/rutas.php
+++ src/rap/php/config/rutas.php
@@ -1,12 +1,7 @@
 <?php
 	define("DIR_WEB", $_SERVER['DOCUMENT_ROOT'] . '/rap/' ); 
 	define("DIR_GLOBAL_LIB", $_SERVER['DOCUMENT_ROOT'] . '/lib/' );
-	define("DIR_LIB", DIR_WEB . 'recursos/lib/' );
-
-	$datos_bd = Array(
-		'host' 			=> 'host',
-		'usuario' 		=> 'usuario',
-		'contrasenna' 		=> 'contrasenna',
-		'bd' 			=> 'bd'
-	);
+	define("DIR_CONFIG", DIR_WEB . 'php/config/' );
+	define("DIR_LIB", DIR_WEB . 'php/lib/' );
+	define("DIR_CLASES", DIR_WEB . 'php/clases/' );
 ?>
