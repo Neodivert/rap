1acff44717e1e0ad0967117ea8d295789db5ac9d
Neodivert <neodivert@gmail.com>
2013-03-25 18:03:05 +0000

    Update bd.php
    - Se ha añadido una cabecera con la descripción y la licencia para el fichero de configuración "bd.php".

--- src/php/config/bd.php
+++ src/php/config/bd.php
@@ -1,4 +1,25 @@
 <?php
+	/***
+	 bd.php
+	 Datos de la conexion a la BD.
+	 Copyright (C) Moises J. Bonilla Caraballo 2012 - 2013.
+	****
+	 This file is part of RAP.
+
+	 RAP is free software: you can redistribute it and/or modify
+	 it under the terms of the GNU General Public License as published by
+	 the Free Software Foundation, either version 3 of the License, or
+	 (at your option) any later version.
+
+	 RAP is distributed in the hope that it will be useful,
+	 but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 GNU General Public License for more details.
+
+	 You should have received a copy of the GNU General Public License
+	 along with RAP.  If not, see <http://www.gnu.org/licenses/>.
+	***/
+	
 	$datos_bd = Array(
 		'host' 			=> 'host',
 		'usuario' 		=> 'usuario',
